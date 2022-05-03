6f79f520d90ad071aed570e46bcddac11c624378
Rain <29941722+AlicornIndustries@users.noreply.github.com>
2022-05-02 21:44:28 -0500

    Basic Game class

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,35 @@
 import * as ROT from "./lib/index.js"
+// LEARN: optimize/tree-shaking imports
 
-let display = new ROT.Display({width:80, height:20});
-let container = display.getContainer() as HTMLElement;
+// LEARN: better pattern for this? Non-singleton?
+export class Game  {
+    private _display: ROT.Display;
+    get display(): ROT.Display {return this._display}
 
-document.body.appendChild(container);
+    constructor(width=80,height=24) {
+        this._display = new ROT.Display({width,height})
+    }
+}
 
-console.log("Hello, World!");
+let Bongo = {
+    num: 10
+}
+
+// LEARN: check if this is still up-to-date
+window.onload = function() {
+
+    let game = new Game();
+
+    document.body.appendChild(game.display.getContainer() as HTMLElement);
+    
+}
+
+// NEXT:
+/*
+https://stackoverflow.com/questions/15335474/multiple-files-making-up-type-script-project
+
+Or just:
+screens.ts
+import {Game} from "./game"
+
+*/
